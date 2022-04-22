2e098a76be262d93952dc7ef1527dd6e6f4fa73d
Dimitar <dimemilkov1@gmail.com>
2022-04-22 19:24:45 +0200

    Emaily

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,17 @@
+const req = require('express/lib/request');
+const passport = require('passport');
+
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+};
